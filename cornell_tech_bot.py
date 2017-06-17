3f8ae33f467dcdbf0a654743df571592d11bcad2
James Leung <leungJ.417@gmail.com>
2017-06-17 16:11:24 -0400

    Implemented Read the hello message

--- cornell_tech_bot.py
+++ cornell_tech_bot.py
@@ -4,18 +4,15 @@ from slackclient import SlackClient
 class CornellTechBot:
 	def __init__(self):
 		self.BOT_NAME = 'cornelltechbot'
-		self.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+		self.client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 		self.BOT_ID = SlackClient(os.environ.get('BOT_ID'))
 
-# if __name__ == "__main__":
-# 	api_call = slack_client.api_call("users.list")
-# 	if api_call.get('ok'):
-# 		users = api_call.get('members')
-# 		i = 0
-# 		for user in users:
-# 			i += 1
-# 			if 'name' in user and user.get('name') == BOT_NAME:
-# 				print("Bot ID for '" + user['name'] + "' is " + user.get('id'))	
-# 	else:
-		# print("could not find bot user with the name " + BOT_NAME)
-		
+	def connect(self):
+		if self.client.rtm_connect():
+			return self.client.server
+	
+	def read(self):
+		messages = []
+		while len(messages) == 0:
+			messages = self.client.rtm_read()
+		return messages
--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,19 +1,24 @@
 from unittest import TestCase
-from expects import expect, equal
+from expects import expect, equal, be_a
 
 from cornell_tech_bot import CornellTechBot
+from slackclient._server import Server
 
-ctechbot = None
 
-
-class TestBot(TestCase):
-    def setUp(self):
-	    ctechbot = CornellTechBot()
-
+class TestCornellTechBot(TestCase):
     def test_can_be_created(self):
-        expect(ctechbot.slack_client).not_to(equal(None))
+        ctechbot = CornellTechBot()
+        expect(ctechbot.client).not_to(equal(None))
         expect(ctechbot.BOT_ID).not_to(equal(None))
         expect(ctechbot.BOT_NAME).to(equal("cornelltechbot"))
 
-    def test_can_read_messages(self):
-        expect(ctechbot.connect()).not_to(equal(None))
+    def test_can_connect(self):
+        ctechbot = CornellTechBot()
+        ws_server = ctechbot.connect()
+        expect(ws_server).to(be_a(Server))
+    
+    def test_can_read(self):
+        ctechbot = CornellTechBot()
+        ws_server = ctechbot.connect()
+        hello_message = [{'type': 'hello'}]
+        expect(ctechbot.read()).to(equal(hello_message))
