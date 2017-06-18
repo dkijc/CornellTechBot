3346564532842d9c8d9d2d542af2038e3990a924
Daniel Kim <wwjdat2000@yahoo.com>
2017-06-18 17:19:06 -0400

    Fix infinite loop of replies from CornellTechBot

--- run.py
+++ run.py
@@ -5,5 +5,10 @@ if __name__ == "__main__":
     server = ctechbot.connect()
 
     # TODO: fix infinite loop for read_and_respond
-    while True:
-        ctechbot.read_and_respond()
+    if server:
+        users = ctechbot.client.api_call('users.list')
+        
+        while True:
+            ctechbot.read_and_respond(users)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
